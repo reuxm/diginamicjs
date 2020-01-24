280b518d803f6185c516c3b2f87fb05dcf6df39b
matthias <matthias@KaaK756U.lan>
2020-01-24 16:48:46 +0100

    value < 3 OK

--- introjs/tp1/ex2.js
+++ introjs/tp1/ex2.js
@@ -2,8 +2,10 @@ function fibo() {
 	let nb = document.getElementById('number').value;
 	let e1 = 0;
 	let e2 = 1;
-	document.getElementById('champ').innerHTML += '<p>'+0+'</p>';
-	document.getElementById('champ').innerHTML += '<p>'+1+'</p>';
+	if( nb > 0)
+		document.getElementById('champ').innerHTML += '<p>'+0+'</p>';
+	if( nb > 1)
+		document.getElementById('champ').innerHTML += '<p>'+1+'</p>';
 	var i;
 	for ( i=0 ; i<nb-2 ; i++ ) {
 		e2 = e1+e2;
