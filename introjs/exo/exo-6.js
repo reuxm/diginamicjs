42dffbb90eb2a3ecaaf585a58737e7d1e2d94b38
matthias <matthias@KaaK756U.lan>
2020-01-29 15:47:28 +0100

    ajout async await

--- introjs/exo/exo-6.js
+++ introjs/exo/exo-6.js
@@ -1,3 +1,8 @@
-fetch('https://api.spacexdata.com/v3/launches/latest')
-.then( r => r.json() )
-.then( d => console.log(d) );
+async function f() {
+
+	await fetch('https://api.spacexdata.com/v3/launches/latest')
+	.then( r => r.json() )
+	.then( d => console.log(d) );
+
+}
+f();
